c0739d5b2725a74ac5b3ce4e32108566756356ce
275885445 <275885445@qq.com>
2019-03-19 18:02:43 +0800

    Update _1081.cpp

--- _1081.cpp
+++ _1081.cpp
@@ -4,6 +4,62 @@
 
 _1081::_1081()
 {
+	int N;
+	vector<string> list;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		bool dotFlag = false, numFlag = false , alphaFlag =false,otherFlag=false;
+		string temp;
+		cin >> temp;
+		if (temp.length()<6)
+		{
+			cout << "Your password is tai duan le.";
+		}
+		else 
+		{
+			for (int j = 0; j < temp.length(); j++)
+			{
+				if (temp[j]=='.')
+				{
+					dotFlag = true;
+				}
+				else if (temp[j]-'0'>=0&& temp[j]-'0'<=9)
+				{
+					numFlag = true;
+				}
+				else if((temp[j] >= 'a' && temp[j] <= 'z')||(temp[j] >= 'A' && temp[j] <= 'Z'))
+				{
+					alphaFlag = true;
+				}
+				else
+				{
+					otherFlag = true;
+				}
+				
+			}
+			if (otherFlag)
+			{
+				cout << "Your password is tai luan le.";
+			}else if (dotFlag&&numFlag)
+			{
+				cout << "Your password needs zi mu.";
+			}
+			else if(dotFlag&&alphaFlag)
+			{
+				cout << "Your password needs shu zi.";
+			}
+			else 
+			{
+				cout << "Your password is wan mei.";
+			}
+			
+		}
+		if (i!=N-1)
+		{
+			cout << "\n";
+		}
+	}
 }
 
 
